e6dc5fc5af3e37293693fd12e701a5192b7fe3d8
Seth Daugherty <daugherty.seth@gmail.com>
2016-08-17 01:40:12 +0700

    Added basic Mocha tests

--- meadowlark.js
+++ meadowlark.js
@@ -12,6 +12,11 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 app.get('/', function(req, res) {
     res.render('home');
 });
@@ -26,7 +31,10 @@ var fortunes = [
 ];
 app.get('/about', function(req, res){
     var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { 
+        fortune: randomFortune,
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
 
 // custom 404 page
